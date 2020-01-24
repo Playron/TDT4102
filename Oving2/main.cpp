549ba21c711fcb1e4cc2d08750e1d8a007232518
Per Haagensen <pdhaagensen@gmail.com>
2020-01-24 12:54:20 +0100

    first commit

--- Oving2/main.cpp
+++ Oving2/main.cpp
@@ -1,15 +1,73 @@
-//
-//  main.cpp
-//  Oving2
-//
-//  Created by Per Haagensen on 24/01/2020.
-//  Copyright © 2020 Per Haagensen. All rights reserved.
-//
-
 #include <iostream>
+using namespace std;
+
+void inputAndPrintInteger(){
+    int i;
+    cout << "Skriv inn et tall: ";
+    cin >> i;
+    cout << "Tallet du skrev er: " << i << "\n";
+}
+
+int inputInteger(){
+    int i;
+    cout << "Skriv inn et tall: ";
+    cin >> i;
+    return i;
+}
+
+void inputIntegersAndPrintSum(){
+    int num1 = inputInteger();
+    int num2 = inputInteger();
+    int sum = num1 + num2;
+    cout << "Summen av tallene: " << sum << "\n";
+}
+
+bool isOdd(int n){
+    if(n%2 != 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+void printHumanReadableTime(int sec){
+    int time = sec%(60*60);
+    cout << time << "\n";
+}
+
+
+int loopSum(int n){
+    int sum = 0;
+    for(int i = 0; i <= n; i++){
+        sum += i;
+    }
+    return sum;
+}
+
+
+void printIntNotNull(){
+    int i;
+    cout << "Tast inn tall: ";
+    cin >> i;
+    int sum = i;
+    while (i != 0) {
+        cout << i;
+        cout << "\n" << "Tast inn tall: ";
+        cin >> i;
+        sum += i;
+    }
+    cout << "Sum: " << sum << "\n";
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//    inputAndPrintInteger();
+//    int number = inputInteger();
+//    cout << "Du skrev: " << number << "\n";
+//    inputIntegersAndPrintSum();
+//    for(int i = 0; i <15; i++){
+//        cout << i  << " " << boolalpha << isOdd(i) << "\n";
+//    }
+//    printHumanReadableTime(3600);
+//    cout << "Sum: " << loopSum(100) << "\n";
+    printIntNotNull();
 }
