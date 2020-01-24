1bd1bc20344d09ddeccd26e1ae4d5af3e30705b5
Per Haagensen <pdhaagensen@gmail.com>
2020-01-24 11:41:42 +0100

    Initial Commit

---
+++ Oving2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Oving2
+//
+//  Created by Per Haagensen on 24/01/2020.
+//  Copyright © 2020 Per Haagensen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
